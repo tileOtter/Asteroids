c93ebca527fe80480415b8f3ecc07322b4da5a05
tileotter <adamfoster913@gmail.com>
2025-09-26 16:45:24 -0400

    limited to 60 fps

--- Asteroids/main.py
+++ Asteroids/main.py
@@ -5,13 +5,16 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
         pygame.display.flip()
+        clock.tick(60)
+        dt = clock.tick() / 1000
 
 
 if __name__ == "__main__":
