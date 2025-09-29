579536a2ae1550a4e1873a25dbd6239bdf50280f
tileotter <adamfoster913@gmail.com>
2025-09-29 15:57:29 -0400

    changed the main loop to update and draw on the containers updatable, drawable. Added Player class to both containers.

--- Asteroids/main.py
+++ Asteroids/main.py
@@ -7,15 +7,19 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}") 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+        updatable.update(dt)
+        for sprite in drawable:
+            sprite.draw(screen)
         pygame.display.flip()
         clock.tick(60)
         dt = clock.tick(60) / 1000
