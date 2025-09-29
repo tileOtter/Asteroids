23de1a0e084373725939e6a3228edccf659055e1
tileotter <adamfoster913@gmail.com>
2025-09-29 15:25:08 -0400

    Added player.py, defines Player child class. Added player model to game as a triangle and added the ability to rotate left and right using 'a' and 'd'.

--- Asteroids/main.py
+++ Asteroids/main.py
@@ -1,20 +1,25 @@
 import pygame
 from constants import *
+from player import Player
 def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    print(f"Screen height: {SCREEN_HEIGHT}") 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.update(dt)
+        player.draw(screen)
         pygame.display.flip()
         clock.tick(60)
-        dt = clock.tick() / 1000
+        dt = clock.tick(60) / 1000
+        
 
 
 if __name__ == "__main__":
