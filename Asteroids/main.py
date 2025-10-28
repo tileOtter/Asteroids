63766ef5a6cc961b60a7f5e916eedc61f554f416
tileotter <adamfoster913@gmail.com>
2025-10-28 12:59:08 -0400

    Added shot class and created the player input to shoot using the space bar.

--- Asteroids/main.py
+++ Asteroids/main.py
@@ -4,6 +4,7 @@ from constants import *
 from asteroidfield import AsteroidField
 from player import Player
 from asteroid import Asteroid
+from shot import Shot
 
 def main():
     print("Starting Asteroids!")
@@ -15,9 +16,11 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
     Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = (updatable)
+    Shot.containers = (shots, updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     asteroidfield = AsteroidField()
     while True:
--- Asteroids/player.py
+++ Asteroids/player.py
@@ -1,6 +1,7 @@
 import pygame
 from circleshape import CircleShape
 from constants import *
+from shot import Shot
 
 class Player(CircleShape):
     def __init__(self, x, y):
@@ -25,6 +26,11 @@ class Player(CircleShape):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         self.position += forward * PLAYER_SPEED * dt
 
+    def shoot(self):
+        shot = Shot(self.position.x, self.position.y + 1, SHOT_RADIUS)
+        shotvel = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+        shot.velocity = shotvel
+
     def update(self, dt):
         keys = pygame.key.get_pressed()
 
@@ -36,3 +42,5 @@ class Player(CircleShape):
             self.move(dt)
         if keys[pygame.K_s]:
             self.move(-dt)
+        if keys[pygame.K_SPACE]:
+            self.shoot()
