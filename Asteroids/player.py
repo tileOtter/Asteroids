c3c9736203802aadc967e71a0a881a8169335584
tileotter <adamfoster913@gmail.com>
2025-10-28 13:12:09 -0400

    Added rate limit of .3 seconds to players shots.

--- Asteroids/player.py
+++ Asteroids/player.py
@@ -7,6 +7,7 @@ class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.timer = 0
 
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -43,4 +44,8 @@ class Player(CircleShape):
         if keys[pygame.K_s]:
             self.move(-dt)
         if keys[pygame.K_SPACE]:
-            self.shoot()
+             while self.timer <= 0:
+                self.shoot()
+                self.timer = PLAYER_SHOOT_COOLDOWN
+        
+        self.timer -= dt
